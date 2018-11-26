56041e49f10dff93743239b3aea6464f3c5b700c
Railly Koeiman <railly6@live.nl>
2018-11-26 10:45:44 +0100

    Made friends more compact

--- src/friends.js
+++ src/friends.js
@@ -8,42 +8,38 @@ module.exports = (io, socket, args) => {
 
 	let userId = clientData[socket.id].userid;
 
-	if(args.type == 'get') {
-		Friends.findAll({
-			where: {
-				[Op.or]: [
-					{userId:   userId},
-					{friendId: userId}
-				]
-			} 
-		}).then(async friends => {
-			let results = [];
+	if(args.type == 'get') Friends.findAll({
+		where: {
+			[Op.or]: [
+				{userId:   userId},
+				{friendId: userId}
+			]
+		} 
+	}).then(async friends => {
+		let results = [];
 
-			for (let i = 0; i < friends.length; i++) {
-				if(friends[i].userId == userId) await Users.find({ where: {id: friends[i].friendId} }).then(user => results.push({
-					id:       user.id,
-					username: user.username
-				}));
-				else await Users.find({ where: {id: friends[i].userId} }).then(user => results.push({
-					id:       user.id,
-					username: user.username
-				}));
-			}
+		for (let i = 0; i < friends.length; i++) {
+			if(friends[i].userId == userId) await Users.find({ where: {id: friends[i].friendId} }).then(user => results.push({
+				id:       user.id,
+				username: user.username
+			}));
+			else await Users.find({ where: {id: friends[i].userId} }).then(user => results.push({
+				id:       user.id,
+				username: user.username
+			}));
+		}
 
-			socket.emit('friend-add', results);
-		});
+		socket.emit('friends-add', friends.length ? results : false);
+	});
 
-	} else if(args.type == 'add') {
-		console.log('Adding user');
-		Users.find({ where: {id: clientData[socket.id].userid} }).then(user => {
-			Users.find({ where: {id: args.friendId} }).then(friend => {
-				socket.send('friend-add', [friend]);
-				Friends.create({
-					userId:   user.id,
-					friendId: friend.id,
-					status: 1
-				});
+	else if(args.type == 'add') Users.find({ where: {id: clientData[socket.id].userid} }).then(user => {
+		Users.find({ where: {id: args.friendId} }).then(friend => {
+			socket.send('friends-add', [friend]);
+			Friends.create({
+				userId:   user.id,
+				friendId: friend.id,
+				status: 1
 			});
 		});
-	}
+	});
 }
