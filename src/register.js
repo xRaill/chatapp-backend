b2d2de9beb8e1f696b78b5aed9a6a5a09b7e588d
Railly Koeiman <railly6@live.nl>
2018-11-27 11:33:14 +0100

    Register now uses callbacks and logs you in

--- src/register.js
+++ src/register.js
@@ -1,37 +1,51 @@
 let bcrypt = require('bcryptjs');
 
-module.exports = (io, socket, args) => {
+module.exports = (io, socket, args, callback) => {
 
-	let user = mod.model('user');
+	let Users = mod.model('user');
 
-	user.findOne({
-		where: {
-			username: args.username
-		}
-	}).then(res => {
-		if(!res) {
-			// Username does not exist, hash password and create account
-			bcrypt.hash(args.password, bcrypt.genSaltSync(8)).then(password => {
-				user.create({
-					username: args.username,
-					password: password,
-					status: '1'
-				}).then(result => {
-					if(result) socket.emit('register', {success: true});
-					else socket.emit('register', {
-						success: false,
-						type: 'toast',
-						message: 'An error occured'
-					});
-				});
-			});
-		} else {
-			// Username already exists
-			socket.emit('register', {
+	Users.find({ where: {username: args.username}}).then(res => {
+		
+		if(res) return callback({
+			success: false,
+			error:   'error.register.username-already-exists'
+		});
+
+		if(args.username.length < 5 || 18 < args.username.length) return callback({
+			success: false,
+			error:   'error.register.username-invalid'
+		});
+
+		if(!/^[a-z0-9]+$/i.test(args.username)) return callback ({
+			success: false,
+			error:   'error.register.username-invalid'
+		});
+
+		if(args.password.length < 7 || 30 < args.password.length) return callback({
+			success: false,
+			error:   'error.register.password-invalid'
+		});
+
+		bcrypt.hash(args.password, bcrypt.genSaltSync(8)).then(hash => Users.create({
+			username: args.username,
+			password: hash,
+			status: 1
+		}).then(user => {
+
+			if(!user) return callback({
 				success: false,
-				type: 'username',
-				message: 'Username already exists!'
+				error:   'error.database.action'
 			});
-		}
+
+			clientData[socket.id].loggedin = true;
+			clientData[socket.id].username = user.username;
+			clientData[socket.id].userid   = user.id;
+
+			return callback({
+				success:  true,
+				userId:   user.id,
+				username: user.username
+			});
+		}));
 	});
 }
