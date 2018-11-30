f1243d446c70050ba6671af39c4217a3ba07b20a
Railly Koeiman <railly6@live.nl>
2018-11-30 13:30:26 +0100

    Added a logout

---
+++ src/logout.js
@@ -0,0 +1,24 @@
+module.exports = (io, socket, args, callback) => {
+
+	let Tokens = mod.model('tokens');
+
+	Tokens.find({ where: {token: clientData[socket.id].token} }).then(token => {
+
+		if(!token) return callback({
+			success: false,
+			error:  'Can\'t find token'
+		});
+
+		token.update({ status: 9 }).then(token => {
+
+			clientData[socket.id].loggedin = false;
+			clientData[socket.id].userid   = null;
+			clientData[socket.id].username = 'Anonymouse';
+			clientData[socket.id].token    = null;
+
+			return callback({
+				success: true
+			});
+		});
+	});
+}
