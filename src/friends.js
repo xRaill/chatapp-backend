eb55c78601a23a677ba2b5b11834703a485edb0f
Railly Koeiman <railly6@live.nl>
2018-11-30 11:15:06 +0100

    Added friend requests

--- src/friends.js
+++ src/friends.js
@@ -1,45 +1,98 @@
-module.exports = (io, socket, args) => {
+module.exports = (io, socket, args, callback) => {
 
-	if(!clientData[socket.id].loggedin) return socket.emit('logout');
-
-	let Users = mod.model('user');
 	let Friends = mod.model('friends');
-	let Op = Sequelize.Op;
+	let Users   = mod.model('user');
 
 	let userId = clientData[socket.id].userid;
+	let Op = Sequelize.Op;
 
 	if(args.type == 'get') Friends.findAll({
 		where: {
-			[Op.or]: [
-				{userId:   userId},
-				{friendId: userId}
-			]
-		} 
+			[Op.or]: [{userId: userId}, {friendId: userId}],
+			status:  {[Op.or]: [0, 1]}
+		}
 	}).then(async friends => {
 		let results = [];
 
 		for (let i = 0; i < friends.length; i++) {
 			if(friends[i].userId == userId) await Users.find({ where: {id: friends[i].friendId} }).then(user => results.push({
 				id:       user.id,
+				request:  false,
 				username: user.username
 			}));
 			else await Users.find({ where: {id: friends[i].userId} }).then(user => results.push({
 				id:       user.id,
+				request:  friends[i].status ? true : false,
 				username: user.username
 			}));
 		}
 
-		socket.emit('friends-add', friends.length ? results : false);
+		return callback({
+			success: true,
+			friends: results.length ? results : false
+		});
 	});
 
-	else if(args.type == 'add') Users.find({ where: {id: clientData[socket.id].userid} }).then(user => {
-		Users.find({ where: {id: args.friendId} }).then(friend => {
-			socket.send('friends-add', [friend]);
-			Friends.create({
-				userId:   user.id,
-				friendId: friend.id,
-				status: 1
-			});
+	else if(args.type == 'request') Friends.find({
+		where: {
+			userId:   userId,
+			friendId: args.friendId,
+			status:   {[Op.or]: [0, 1]}
+		}
+	}).then(friend => {
+		
+		if(friend && friend.status == 1) return callback({
+			success: false,
+			error:  'Already friends.'
 		});
+
+		if(friend && friend.status == 0) return callback({
+			success: false,
+			error:  'Friend request already send'
+		});
+
+		Users.find({ where: {id: args.friendId} }).then(user => Friends.create({
+			userId:   user.id,
+			friendId: friend.id,
+			status:   0
+		}).then(friend => {
+
+			let socketId = Object.entries(clientData).find(a => a[1].userid == user.id);
+			if(socketId) io.sockets.connected[socketId].socket.emit('friends-request', user);
+
+			return callback({
+				success: true
+			});
+		}));
+	});
+
+	else if(args.type == 'accept') Friends.find({ where: {userId: args.friendId, friendId: userId, status: 0} }).then(friend => {
+
+		if(!friend) return callback({
+			success: false,
+			error:  'Friend request not found'
+		});
+
+		friend.update({ status: 1 }).then(friend => Users.find({ where: {id: friend.friendId} }).then(user => {
+
+			let socketId = Object.entries(clientData).find(a => a[1].userid == friend.userId);
+			if(socketId) io.sockets.connected[socketId].socket.emit('friends-accept', user);
+
+			return callback({
+				success: true
+			});
+		}));
+	});
+
+	else if(args.type == 'deny') Friends.find({ where: {userId: args.friendId, friendId: userId, status: 0} }).then(friend => {
+
+		if(!friend) return callback({
+			success: true,
+			error:  'Friend request not found'
+		});
+
+		friend.update({ status: 9 }).then(friend => callback({
+			success: true
+		}));
 	});
 }
