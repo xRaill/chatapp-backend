31f36e6f09f2bc80f5fafc8dac35823a7d5494c9
Railly Koeiman <railly6@live.nl>
2019-01-23 10:35:30 +0100

    Removed alot of commented lines

--- index.js
+++ index.js
@@ -42,26 +42,3 @@ mod.action('database', () => {
 	});
 
 });
-
-
-
-
-
-
-// io.on('connection', (socket) => {
-// 	console.log(socket.id +' connected');
-// 	clientData[socket.id] = {
-// 		loggedin: false,
-// 		username: 'Anonymous'
-// 	};
-// 	socket.emit('connected');
-// 	socket.on('action', (type, args = {}, callback = () => {}) => mod.action(type, io, socket, args, callback));
-// 	socket.on('toastall', () => io.emit('toast'));
-// 	socket.on('clientdata', () => socket.emit('test', clientData));
-// 	socket.on('disconnect', () => delete clientData[socket.id]);
-// });
-
-// app.listen(8080, () => {
-// 	console.log('listening on *:8080');
-// 	(mod.action('database'))();
-// });
